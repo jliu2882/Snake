bcc5a4aa2cb42bc1ca60b9019de0bb50376c6f0d
Edmund Wu <fangkazuto@gmail.com>
2019-02-05 16:11:49 -0500

    Allow for body tracking

--- src/Snake.java
+++ src/Snake.java
@@ -1,9 +1,25 @@
 public class Snake extends Entity {
+    private Snake lastPart;
+
     public Snake(Position position) {
         super(position);
     }
 
+    public Snake(Position position, Snake lastPart) {
+        super(position);
+        this.lastPart = lastPart;
+    }
+
     public Snake(int x, int y) {
         super(x, y);
+    }
+
+    public Snake(int x, int y, Snake lastPart) {
+        super(x, y);
+        this.lastPart = lastPart;
+    }
+
+    public Snake getLastPart() {
+        return lastPart;
     }
 }
