e5c590f1da8d65d58b03f35661df46c4fdb10e6e
Edmund Wu <fangkazuto@gmail.com>
2019-02-05 15:53:02 -0500

    Adjust for 0 based indexes

--- src/Constants.java
+++ src/Constants.java
@@ -5,11 +5,11 @@ public class Constants {
     public static final int BOARD_X = 20;
     public static final int BOARD_Y = 20;
 
-    public static final int SNAKE_INITIAL_X = Constants.BOARD_X / 5;
-    public static final int SNAKE_INITIAL_Y = Constants.BOARD_Y / 2;
+    public static final int SNAKE_INITIAL_X = Constants.BOARD_X / 2 - 1;
+    public static final int SNAKE_INITIAL_Y = Constants.BOARD_Y / 5 - 1;
 
-    public static final int FOOD_INITIAL_X = Constants.SNAKE_INITIAL_X * 4;
-    public static final int FOOD_INITIAL_Y = Constants.SNAKE_INITIAL_Y;
+    public static final int FOOD_INITIAL_X = Constants.SNAKE_INITIAL_X;
+    public static final int FOOD_INITIAL_Y = Constants.SNAKE_INITIAL_Y * 4 - 1;
 
     public static final double SCALE_X = ((double) Constants.SCREEN_X) / ((double) Constants.BOARD_X);
     public static final double SCALE_Y = ((double) Constants.SCREEN_Y) / ((double) Constants.BOARD_Y);
