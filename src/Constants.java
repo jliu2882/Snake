ad3ca8df9e29ef8d7fdd44e5fd45386a5790281e
Edmund Wu <fangkazuto@gmail.com>
2019-02-04 22:16:49 -0500

    Reorganize Constants

--- src/Constants.java
+++ src/Constants.java
@@ -1,28 +1,10 @@
-import javafx.stage.Stage;
-
 public class Constants {
-    public Stage stage;
-    public Constants(Stage stage){
-        this.stage = stage;
-    }
-    public void update(){
-        HEIGHT2 = stage.getY();
-        WIDTH2 = stage.getX();
-        SCALEX = WIDTH2/WIDTH;
-        SCALEY = HEIGHT2/HEIGHT;
-    }
+    public static final int SCREEN_X = 1000;
+    public static final int SCREEN_Y = 1000;
 
-    /** These constants can be changed freely */
-    // dimensions of the board
-    public static final int HEIGHT = 10;
-    public static final int WIDTH = 10;
-    //dimensions of the popup screen
-    public static double HEIGHT2 = 1000;
-    public static double WIDTH2 = 1000;
+    public static final int BOARD_X = 10;
+    public static final int BOARD_Y = 10;
 
-    /** These constants should not be touched */
-    public static double SCALEX = WIDTH2/WIDTH;
-    public static double SCALEY = HEIGHT2/HEIGHT;
-
-
+    public static final double SCALE_X = ((double) Constants.SCREEN_X) / ((double) BOARD_X);
+    public static final double SCALE_Y = ((double) Constants.SCREEN_Y) / ((double) BOARD_Y);
 }
