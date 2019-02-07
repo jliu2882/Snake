dc30ca891ade5ba53878ebbe5fd30aeb3d173522
Edmund Wu <fangkazuto@gmail.com>
2019-02-06 21:34:04 -0500

    Snake.lastPart -> Snake.nextPart
    Propagate to methods as well and introduce respective setter,
    Snake.setNextPart

--- src/Snake.java
+++ src/Snake.java
@@ -1,27 +1,31 @@
 public class Snake extends Entity {
-    private Snake lastPart;
+    private Snake nextPart;
     private Direction direction = Directions.RIGHT;
 
     public Snake(Position position) {
         super(position);
     }
 
-    public Snake(Position position, Snake lastPart) {
+    public Snake(Position position, Snake nextPart) {
         super(position);
-        this.lastPart = lastPart;
+        this.nextPart = nextPart;
     }
 
     public Snake(int x, int y) {
         super(x, y);
     }
 
-    public Snake(int x, int y, Snake lastPart) {
+    public Snake(int x, int y, Snake nextPart) {
         super(x, y);
-        this.lastPart = lastPart;
+        this.nextPart = nextPart;
     }
 
-    public Snake getLastPart() {
-        return lastPart;
+    public Snake getNextPart() {
+        return nextPart;
+    }
+
+    public void setNextPart(Snake nextPart) {
+        this.nextPart = nextPart;
     }
 
     public Direction getDirection() {
